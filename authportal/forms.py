36254eda3280e72e1c3f9fd13f0a2c0db50fa86f
Monirul Islam <monirul.developers@gmail.com>
2024-08-23 10:51:21 +0600

    ev

--- authportal/forms.py
+++ authportal/forms.py
@@ -5,25 +5,30 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
-class CustomSignUpForm(UserCreationForm):
-    password1 = forms.CharField(
-        label="Password",
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter password'})
-    )
-    password2 = forms.CharField(
-        label="Confirm password",
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm password'})
-    )
+# class CustomSignUpForm(UserCreationForm):
+#     password1 = forms.CharField(
+#         label="Password",
+#         widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter password'})
+#     )
+#     password2 = forms.CharField(
+#         label="Confirm password",
+#         widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm password'})
+#     )
 
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', 'password1', 'password2')
+
+#     def clean_password2(self):
+#         password1 = self.cleaned_data.get("password1")
+#         password2 = self.cleaned_data.get("password2")
+#         if password1 and password2 and password1 != password2:
+#             raise forms.ValidationError("Passwords do not match")
+#         return password2
+
+
+class CustomSignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords do not match")
-        return password2
-
+        fields = ('username','email','first_name', 'last_name')
 
